99a63fe3f59aa04f3a38e00b3b3828211e071852
Mohamad Termanini <mohtermanini.job@gmail.com>
2022-03-31 01:05:04 +0300

    Add github account link

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,13 +3,14 @@
         <div class="container-fluid flex-column pt-3 px-5 pb-3">
             <div class="row m-0 w-100">
                 <div class="col-md-6 mb-2">
-                     <a href="{{ route('index') }}" class="navbar-brand">صحتي</a> <small
+                    <a href="{{ route('index') }}" class="navbar-brand">صحتي</a> <small
                         class="d-block pt-2 text-white">المنصة الطبية العربية الأكبر التي تُعنى بتقديم المحتوى الطبي
-                        الموثوق، بأقلام آلاف الأطباء المعتمدين…</small> </div>
+                        الموثوق، بأقلام آلاف الأطباء المعتمدين…</small>
+                </div>
                 <div class="col-md-6 d-flex justify-content-end  mb-2">
-                   <!-- 
+                    <!--
                     <ul class="navbar-nav flex-column">
-                        <li class="nav-item"> <a href="" class="nav-link">تواصل معنا</a> </li> 
+                        <li class="nav-item"> <a href="" class="nav-link">تواصل معنا</a> </li>
                         <li class="nav-item"> <a href="" class="nav-link">عن صحتي</a> </li>
                     </ul>
                     -->
@@ -17,10 +18,17 @@
             </div>
             <div class="row mt-3">
                 <div class="col">
-                        <p class="mb-0 text-white" style="direction: ltr;">
-                            &copy; 2022 Mohamad Termanini
-                        </p>
-                    </div>
+                    <p class="mb-0 text-white" style="direction: ltr;">
+                        &copy; 2022 Mohamad Termanini
+                        <a href="https://github.com/mohtermanini" target="_blank" class="text-white p-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-github" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                            </svg>
+                        </a>
+                    </p>
+                </div>
             </div>
         </div>
     </nav>
